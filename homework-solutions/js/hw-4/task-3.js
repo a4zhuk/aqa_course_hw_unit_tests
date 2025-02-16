62eeff68e4de147da7fa0bea37be110afc2491b7
a4zhuk <san9zhuk@gmail.com>
2025-02-16 21:30:54 +0300

    convert age to number

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -12,10 +12,13 @@ let age = "10";
 let userName = "Sasha";
 if (isNaN(age)){
   console.log("Incorrect data type");
-} else if (age < minAge){
+} else {
+  age = +age 
+  if (age < minAge){
   console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
 } else if (age >= minAge && age <= maxAge){
   console.log("Welcome " + userName + "!");
 } else if (age > maxAge){
   console.log("Keep calm and look Culture channel");
 } else console.log("Technical work");
+}
