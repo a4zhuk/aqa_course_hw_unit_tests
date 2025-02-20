b4bd20714a03ee65e71258167191584e3de5e4ed
a4zhuk <san9zhuk@gmail.com>
2025-02-20 14:35:13 +0300

    add check on consonants in task3

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -5,26 +5,30 @@
  */
 const word = 'hello';
 let vovelsList ="aeiouy"
+let consonantsList = 'bcdfghjklmnpqrstvwxyz'
 let vowelsCount = 0;
 let consonantsCount = 0;
 for (let i = 0; i < word.length; i++){
   if (vovelsList.includes(word[i])){
     vowelsCount +=1;
-  } else if (word[i] ==" "){
-  } else consonantsCount += 1;
+  } else if (consonantsList.includes(word[i].toLowerCase())){
+    consonantsCount += 1
+  }
 }
 let vowelsAndConsonantsResult = `${word} contains ${vowelsCount} vowels and ${consonantsCount} consonants`;
 console.log(vowelsAndConsonantsResult)
 // другой вариант
-let word1 = "hello";
+let word1 = "HeLlo-'`123";
 let vowelsArr = ['a', 'e','i', 'o', 'u', 'y']
+let consonantsArr = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
 let counter1 = 0;
 let counter2 = 0;
 for (let i = 0; i < word1.length; i++){
-  if (vowelsArr.includes(word1[i])){
+  if (vowelsArr.includes(word1[i].toLowerCase())){
     counter1 +=1;
-  } else if (word1[i] ==" "){
-  } else counter2 += 1
+  } else if (consonantsArr.includes(word1[i].toLowerCase())){
+    counter2 += 1
+  }
 }
 let result = (`${word1} contains ${counter1} vowels and ${counter2} consonants`)
 console.log(result)
