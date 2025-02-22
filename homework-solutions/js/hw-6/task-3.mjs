6bbb4ae016fd9a7c3522d9cadd224efde919338b
a4zhuk <san9zhuk@gmail.com>
2025-02-22 14:09:15 +0300

    homework-6

--- homework-solutions/js/hw-6/task-3.mjs
+++ homework-solutions/js/hw-6/task-3.mjs
@@ -6,6 +6,11 @@
 
   Присвойте результат в переменную "unique"
 */
-let unique;
-
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
+let unique =[];
+for(let num of arr){
+  if (!unique.includes(num)){
+    unique.push(num);
+  }
+}
 export { unique };
